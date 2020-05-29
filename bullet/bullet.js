a84c053064820c30e6bddfb1e9b543ee781afb8d
chenyajun <chenyajun@migu.cn>
2020-05-29 15:50:31 +0800

    add

--- bullet/bullet.js
+++ bullet/bullet.js
@@ -13,7 +13,7 @@
 
 registerLayout('bullet', class {
   static get inputProperties() {
-    return [ '--padding', '--columns', '--bullet-direction'];
+    return [ '--bullet-direction'];
   }
   static get childInputProperties() { 
     return ['margin-left', 'margin-right', 'margin-top', 'margin-bottom', '--bullet-play-mode'];
@@ -21,6 +21,7 @@ registerLayout('bullet', class {
 
   // async intrinsicSizes() { /* TODO implement :) */}
   async intrinsicSizes(children, edges, styleMap) {
+      // 计算每个元素的最大内容大小
       const childrenSizes = await Promise.all(children.map((child) => {
           return child.intrinsicSizes();
       }));
@@ -36,20 +37,22 @@ registerLayout('bullet', class {
       return {maxContentSize, minContentSize};
   }
   async layout(children, edges, constraintSpace, styleMap) {
-      const childFragments = [];
-      let inlineOffset = 0;
-      let blockOffset = 0;
+      const childFragments = []; // 所有的子元素
+      let inlineOffset = 800; // 默认x偏移量
+      let inlineOffset4Scroll = 0;
+      let blockOffset = 0; // 默认y偏移量
       let maxBlockSizeInRow = 0;
+      let rowNum = 5; // 根据子元素高度划分的通道数量，默认为5
 
-      let availableInlineSize = constraintSpace.fixedInlineSize - edges.inline;
-      let availableBlockSize = constraintSpace.fixedBlockSize - edges.block;
+      let availableInlineSize = constraintSpace.fixedInlineSize - edges.inline; // 容器的可用宽度
+      let availableBlockSize = constraintSpace.fixedBlockSize - edges.block; // 容器的可用高度
       let topBlockOffset = 0;
       let bottomBlockOffset = availableBlockSize;
       let isRepeatAttempt = false;
       let isTopLayout = false;
       let playMode = 'scroll';
       // let childModes = []
-      console.log("constraintSpace:", constraintSpace, 'edges:', edges)
+      // console.log("constraintSpace:", constraintSpace, 'edges:', edges)
 
       // let columnValue = styleMap.get('--bullet-direction');
       // console.log("direction:", columnValue)
@@ -58,8 +61,8 @@ registerLayout('bullet', class {
         let child = children[i];
         // console.log(i, '--', await child.intrinsicSizes(children, edges, styleMap))
         const childRealInlineSize =  await child.intrinsicSizes(children, edges, styleMap)
-        // console.log(i, '--', childRealInlineSize.maxContentSize)
-        
+        // console.log(i, '--', childRealInlineSize)
+
         const leftMargin = child.styleMap.get('margin-left').value;
         const rightMargin = child.styleMap.get('margin-right').value;
         const topMargin = child.styleMap.get('margin-top').value;
@@ -71,26 +74,33 @@ registerLayout('bullet', class {
           // availableInlineSize
           fixedInlineSize: childRealInlineSize.maxContentSize
         });
+
         let childInlineSize = childFragment.inlineSize + leftMargin + rightMargin;
-        
+
+        rowNum = Math.ceil(availableBlockSize / childFragment.blockSize) - 1
+        // console.log(i, '--', rowNum)
+
+
+
         if (playMode == 'top') {
             childFragment.inlineOffset = availableInlineSize/2 - childFragment.inlineSize/2;
-            childFragment.blockOffset = topBlockOffset + topMargin;
-            topBlockOffset += childFragment.blockSize + topMargin + bottomMargin  
+            childFragment.blockOffset = topBlockOffset;
+            topBlockOffset += childFragment.blockSize;
         } else if (playMode == 'bottom') {
             childFragment.inlineOffset = availableInlineSize/2 - childFragment.inlineSize/2;
-            childFragment.blockOffset = bottomBlockOffset - childFragment.blockSize - bottomMargin;
-            bottomBlockOffset -= childFragment.blockSize + topMargin + bottomMargin
+            childFragment.blockOffset = bottomBlockOffset - childFragment.blockSize;
+            bottomBlockOffset -= childFragment.blockSize
         } else {
-           if (inlineOffset > availableInlineSize && !isRepeatAttempt) {
+           if (inlineOffset4Scroll > availableInlineSize && !isRepeatAttempt) {
                 blockOffset += maxBlockSizeInRow;
                 maxBlockSizeInRow = 0;
-                inlineOffset = 0;
+                inlineOffset = 800;
+                inlineOffset4Scroll = 0;
                 isRepeatAttempt = true;
                 // Restart loop
                 i--;
                 continue;
-            } else if(inlineOffset > availableInlineSize  && isRepeatAttempt) {
+            } else if(inlineOffset4Scroll > availableInlineSize  && isRepeatAttempt) {
               // If the second attempt failed as well, the child is wider than the
               // there’s room. In that case, force it to be laid out at max width.
               childFragment = await child.layoutNextFragment({
@@ -100,10 +110,10 @@ registerLayout('bullet', class {
             isRepeatAttempt = false;
             childFragment.inlineOffset = inlineOffset + leftMargin;
             childFragment.blockOffset = blockOffset + topMargin;
-            inlineOffset += childInlineSize;
+            inlineOffset4Scroll += childInlineSize;
             maxBlockSizeInRow = Math.max(maxBlockSizeInRow, childFragment.blockSize + topMargin + bottomMargin);
         }
-
+        // console.log(i, '--', childFragment)
         childFragments.push(childFragment);
         
       }
